65e7b91ab5c4caf033a673b2079b489a232e571f
npuKoJlucTbl4 <159910054+npuKoJlucTbl4@users.noreply.github.com>
2024-03-14 11:01:31 +0600

    2

---
+++ app/src/main/java/com/example/testapp2/InnerInterface.java
@@ -0,0 +1,9 @@
+package com.example.testapp2;
+
+import androidx.fragment.app.Fragment;
+
+public class InnerInterface extends Fragment {
+    public InnerInterface() {
+        super(R.layout.interface_inner);
+    }
+}
